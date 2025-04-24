90a24d6ca55b44685e7eec3843d7cce37d9b8c43
Lai Dinh Cao Duc <ducldche176120@fpt.edu.vn>
2025-04-24 23:56:45 +0700

    add email worker

--- services/auth.service.js
+++ services/auth.service.js
@@ -2,10 +2,29 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const User = require("../models/user");
 const config = require("../config/server");
+const ResponseHandler = require("../utils/response-handlers");
 
 async function register(data) {
   const hashedPassword = await bcrypt.hash(data.password, 10);
-  const user = await User.create({ ...data, password: hashedPassword });
+  const existingUser = await User.findOne({
+    $or: [
+      { email: data.email },
+      { phone: data.phone },
+      { identity_number: data.identity_number },
+    ],
+  });
+  console.log(existingUser);
+  
+  if (existingUser) {
+    throw new Error("User already exists");
+  }
+  const user = await User.create({
+    ...data,
+    password: hashedPassword,
+    role: "member",
+    is_verified: false, // Set to false by default
+    is_active: true, // Set to true by default
+  });
   return user;
 }
 
@@ -23,7 +42,21 @@ async function login(email, password) {
   return { user, accessToken };
 }
 
+async function changePasswordFirstTime(userId, newPassword) {
+  const hashedPassword = await bcrypt.hash(newPassword, 10);
+  const user = await User.findByIdAndUpdate(
+    userId,
+    { password: hashedPassword, is_verified: true },
+    { new: true },
+  );
+  if (!user) {
+    return ResponseHandler.notFound(null, "User not found");
+  }
+  return user;
+}
+
 module.exports = {
   register,
   login,
+  changePasswordFirstTime,
 };
--- utils/mailer.js
+++ utils/mailer.js
@@ -12,4 +12,5 @@ const transporter = nodemailer.createTransport({
   },
 });
 
+
 module.exports = transporter;
