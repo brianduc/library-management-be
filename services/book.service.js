0e27c6de1ddf43b7e3e22bda386c8779b4616ab5
Lai Dinh Cao Duc <ducldche176120@fpt.edu.vn>
2025-04-25 13:55:22 +0700

    edit api

--- services/book.service.js
+++ services/book.service.js
@@ -1,35 +1,23 @@
 const Book = require("../models/book");
 
 
-const PAGE_SIZE = 10;
-
 async function getAllBookByPage(page) {
   try {
-    const skip = (page - 1) * PAGE_SIZE;
-    const books = await Book.find()
-      .skip(skip)
-      .limit(PAGE_SIZE);
-    const total = await Book.countDocuments(); 
-    const totalPages = Math.ceil(total / PAGE_SIZE);
-    return {
-      currentPage: page,
-      totalPages,
-      totalBooks: total,
-      books
-    };
+    const books = await Book.find({});
+    return books;
   } catch (err) {
-    console.error('Lỗi khi lấy danh sách sách:', err);
+    console.error("Lỗi khi lấy danh sách sách:", err);
     throw err;
   }
 }
 
 async function getAllBooksv2() {
-  return await Book.find({})
+  return await Book.find({});
 }
 
 async function getBookByTitle(title) {
   const book = await Book.findOne({ title });
-  return book;  
+  return book;
 }
 async function getBookById(id) {
   return await Book.findById(id);
@@ -49,12 +37,12 @@ async function deleteBook(id) {
 async function changeStatusBook(id) {
   const book = await Book.findById(id);
   if (!book) {
-    return null; 
+    return null;
   }
   const updatedBook = await Book.findByIdAndUpdate(
     id,
     { is_hidden: !book.is_hidden },
-    { new: true }
+    { new: true },
   );
   return updatedBook;
 }
